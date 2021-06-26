2d538cbea07663c06be552df67839ddfbdfe0d38
Your Name <you@example.com>
2021-06-26 18:05:43 -0400

    user get by a specific id

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -15,6 +15,25 @@ const userController= {
                 res.status(400).json(err);
             });
     },
+    getUserById({params}, res) {
+        User.findOne({_id: params.id})
+            .populate({
+                path: 'thoughts',
+                selet: '-__v'
+            })
+            .select('-__v')
+            .then(dbUserData => {
+                if (!dbUserData) {
+                    res.status(404).json({message: 'No user found with this id!'});
+                    return;
+                }
+                res.json(dbUserData);
+            })
+            .catch(err => {
+                console.log(err);
+                res.status(400).json(err);
+            });
+    },
     createUser( {body}, res) {
         User.create(body)
         .then(dbUserData => res.json(dbUserData))
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,10 +1,14 @@
 const router= require('express').Router();
-const { getAllUser, createUser }= require('../../controllers/user-controller');
+const { getAllUser, createUser, getUserById }= require('../../controllers/user-controller');
 
 router
     .route('/')
     .get(getAllUser)
     .post(createUser);
+
+router
+    .route('/:id')
+    .get(getUserById);
 
 
 module.exports= router;
