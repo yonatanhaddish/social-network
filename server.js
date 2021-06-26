73fd1648b7f8964fd491e5445bccbb1304205fa6
Your Name <you@example.com>
2021-06-26 15:31:53 -0400

    page template

---
+++ server.js
@@ -0,0 +1,20 @@
+const express= require('express');
+const mongoose= require('mongoose');
+
+const app= express();
+const PORT= process.env.PORT || 3006;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network', {
+    useFindAndModify: false,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+mongoose.set('debug', true);
+
+// app.use(require('./routes'));
+
+app.listen(PORT, () => console.log(`Connected to localhost: ${PORT}`));
